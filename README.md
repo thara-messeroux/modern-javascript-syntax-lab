5853ccc09fc26eb12d52558163b04a34c5c95efa
thara-messeroux <tharamesseroux@gmail.com>
2026-03-28 23:50:54 -0400

    feat: complete exercise 4 using spread operator on arrays

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # modern-javascript-syntax-lab
+
 ## Step 1 🐼
 Used map() to create a new array where each number is multiplied by 2.
 
--- app.js
+++ app.js
@@ -12,52 +12,60 @@
 const nums = [13, 87, 2, 89, 12, 4, 90, 63];
 
 // Your code here:
-// Exercise 1 🐼
-
-// map = transform each item into a new one
 const numsTimesTwo = nums.map((num) => {
-  return num * 2; // multiply each number by 2
+  return num * 2;
 });
 
 console.log(numsTimesTwo);
 
-
 // ! Exercise 2:
-// a. Given the provided `pizzaToppings` array, use destructuring to extract 
-//    the first and second values and place them into variables. Name the 
-//    variable that corresponds to the first value `firstIngredient`. Name the 
+// a. Given the provided `pizzaToppings` array, use destructuring to extract
+//    the first and second values and place them into variables. Name the
+//    variable that corresponds to the first value `firstIngredient`. Name the
 //    variable that corresponds to the second value `secondIngredient`.
 //
 // b. Console log the `firstIngredient` and `secondIngredient` variables.
 //
 // Starting code (don't modify this):
 
-
-// starting array
-const pizzaToppings = ["pepperoni", "cheese", "mushroom"];
+const pizzaToppings = ["Pineapple", "Olives", "Anchovies"];
 
 // Your code here:
-
-// destructuring = pull values out of array into variables
 const [firstIngredient, secondIngredient] = pizzaToppings;
 
 console.log(firstIngredient);
 console.log(secondIngredient);
 
-
 // ! Exercise 3:
+// a. Given the provided `car` object, use destructuring to create two
+//    variables named `make` and `model` that hold the respective values.
+//
+// b. Console log the `make` and `model` variables.
+//
+// Starting code (don't modify this):
 
 const car = {
   make: "Audi",
-  model: "Q5"
+  model: "Q5",
 };
 
 // Your code here:
-
-// Exercise 3 🐯
-
-// destructuring object = pull values by key name
 const { make, model } = car;
 
 console.log(make);
 console.log(model);
+
+// ! Exercise 4:
+// a. Duplicate the provided `morePizzaToppings` array using the spread
+//    operator and assign it to a variable named `uncontroversialPizzaToppings`.
+//
+// b. Console log the `uncontroversialPizzaToppings` variable.
+//
+// Starting code (don't modify this):
+
+const morePizzaToppings = ["Cheese", "Sauce"];
+
+// Your code here:
+const uncontroversialPizzaToppings = [...morePizzaToppings];
+
+console.log(uncontroversialPizzaToppings);
